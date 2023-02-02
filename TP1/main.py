a047232359901f789302fcf2a0cbdb8fbca4bf8a
Thomas Caron <thomas.caron@polymtl.ca>
2023-02-02 12:34:13 -0500

    Modified main

--- TP1/main.py
+++ TP1/main.py
@@ -4,9 +4,7 @@ import numpy as np
 
 from algorithms.utils import parse_matrix, multiply
 from algorithms.strassen import strassen_multiply
-from algorithms.strassen_np import strassen_multiply as strassen_multiply_np
 from algorithms.strassen_leaf import strassen_multiply as strassen_multiply_leaf
-from algorithms.strassen_np_leaf import strassen_multiply as strassen_multiply_np_leaf
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -23,13 +21,9 @@ if __name__ == "__main__":
     np_A, np_B = np.array(A), np.array(B)
 
     t = timeit.timeit('multiply(A, B)', globals=globals(), number=1)
-    # t1 = timeit.timeit('strassen_multiply(A, B)', globals=globals(), number=1)
-    # t2 = timeit.timeit('strassen_multiply_np(np_A, np_B)', globals=globals(), number=1)
+    t2 = timeit.timeit('strassen_multiply(A, B)', globals=globals(), number=1)
     t3 = timeit.timeit('strassen_multiply_leaf(A, B)', globals=globals(), number=1)
-    # t4 = timeit.timeit('strassen_multiply_np_leaf(np_A, np_B)', globals=globals(), number=1)
 
     print("Regular multiply: ", t)
-    # print("Strassen multiply: ", t1)
-    # print("Strasseb multiply with np : ", t2)
+    print("Strassen multiply: ", t2)
     print("Strassen multiply with leaf: ", t3)
-    # print("Strassen multiply with np and leaf: ", t4)
