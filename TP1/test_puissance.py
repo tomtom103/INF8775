82771ac7259196d70d76d28310976e03aa587772
Thomas Caron <thomas.caron@polymtl.ca>
2023-02-09 12:12:34 -0500

    Ajout test de puissance, pas sur si ca marche

--- TP1/test_puissance.py
+++ TP1/test_puissance.py
@@ -1,24 +1,41 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import math
 import pandas as pd
+import numpy as np
+import seaborn as sns
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("data.csv").groupby(['Algorithm', 'Matrix Size']).mean(numeric_only=True).reset_index()
+df['Actual Size'] = df['Matrix Size'].apply(lambda val: 2 ** val)
 
-print(df)
+classic = df.loc[df['Algorithm'] == 'Classic']
 
-data = [
-    ["Strassen", "8x8", 64, 0.00441924123, "n^3"], 
-    ["Strassen", "8x8", 64, 0.00441924123, "n^3"], 
-    ["Strassen", "8x8", 64, 0.00441924123, "n^3"], 
-    ["Conventional", "4x4", 24, 0.00441924123, "n^3"], 
-    ["Conventional", "4x4", 32, 0.543254325, "1"], 
-    ["Conventional", "4x4", 32, 0.4231432155, "1"], 
-    ["Conventional", "4x4", 32, 0.1432213231, "1"]
-]
+graph = sns.FacetGrid(classic, height=4, aspect=1)
+graph = graph.map(plt.plot, 'Actual Size', 'Execution Time')
+graph.set(xscale='log')
+graph.set(yscale='log')
+graph.add_legend()
+plt.savefig('plots/puissance_classic.png')
 
-x_input = np.linspace(0, 10, 1000) # Execution Time
-y_input = x_input**3
+plt.close()
 
-plt.loglog(x_input, y_input)
+strassen = df.loc[df['Algorithm'] == 'Strassen']
 
-# plt.show()
+graph = sns.FacetGrid(strassen, height=4, aspect=1)
+graph = graph.map(plt.plot, 'Actual Size', 'Execution Time')
+graph.set(xscale='log')
+graph.set(yscale='log')
+graph.add_legend()
+plt.savefig('plots/puissance_strassen.png')
+
+plt.close()
+
+strassen_leaf = df.loc[df['Algorithm'] == 'Strassen with Leaf']
+
+graph = sns.FacetGrid(strassen_leaf, height=4, aspect=1)
+graph = graph.map(plt.plot, 'Actual Size', 'Execution Time')
+graph.set(xscale='log')
+graph.set(yscale='log')
+graph.add_legend()
+plt.savefig('plots/puissance_strassen_leaf.png')
+
+plt.close()
