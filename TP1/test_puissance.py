79742db4e30b00a91842636b69e238359fc09df0
Thomas Caron <thomas.caron@polymtl.ca>
2023-02-16 15:17:46 -0500

    Updated test puissance

--- TP1/test_puissance.py
+++ TP1/test_puissance.py
@@ -4,25 +4,48 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-# df = pd.read_csv("data.csv").groupby(['Algorithm', 'Matrix Size']).mean(numeric_only=True).reset_index()
-df = pd.read_csv("leaf_data.csv").groupby(['Leaf Size', 'Matrix Size']).mean(numeric_only=True).reset_index()
-df['Actual Size'] = df['Matrix Size'].apply(lambda val: 2 ** val)
+df = pd.read_csv("data.csv").groupby(['Algorithm', 'Matrix Size']).mean(numeric_only=True).reset_index()
 
-print(df)
+res = df.pivot(index='Matrix Size', columns='Algorithm', values='Execution Time').reset_index()
+res['Actual Size'] = res['Matrix Size'].apply(lambda x: 2 ** x)
 
-leaf_sizes = [2 ** i for i in range(4, 11)]
 
-for size in leaf_sizes:
-    df2 = df.loc[df['Leaf Size'] == size]
+res = res[['Actual Size', 'Classic', 'Strassen', 'Strassen with Leaf']]
 
-    graph = sns.FacetGrid(df2, height=4, aspect=1)
-    graph = graph.map(plt.plot, 'Actual Size', 'Execution Time')
-    graph.set(xscale='log')
-    graph.set(yscale='log')
-    graph.add_legend()
-    plt.savefig(f'plots/puissance_strassen_leaf_size_{size}.png')
+# plot = sns.lineplot(x='Actual Size', )
+# plot.set(xscale='log')
+# plot.set(yscale='log')
+# plot.add_legend()
+# plt.show()
 
-    plt.close()
+# print(df)
+print(pd.melt(res, ['Actual Size']))
+
+plot = sns.lineplot(x='Actual Size', y='value', hue='Algorithm',
+             data=pd.melt(res, ['Actual Size']))
+
+plot.set(xscale='log')
+plot.set(yscale='log')
+
+plt.savefig("plots/puissance_combined.png")
+
+exit()
+# df = pd.read_csv("leaf_data.csv").groupby(['Leaf Size', 'Matrix Size']).mean(numeric_only=True).reset_index()
+# df['Actual Size'] = df['Matrix Size'].apply(lambda val: 2 ** val)
+
+# leaf_sizes = [2 ** i for i in range(4, 11)]
+
+# for size in leaf_sizes:
+#     df2 = df.loc[df['Leaf Size'] == size]
+
+#     graph = sns.FacetGrid(df2, height=4, aspect=1)
+#     graph = graph.map(plt.plot, 'Actual Size', 'Execution Time')
+#     graph.set(xscale='log')
+#     graph.set(yscale='log')
+#     graph.add_legend()
+#     plt.savefig(f'plots/puissance_strassen_leaf_size_{size}.png')
+
+#     plt.close()
 
 # classic = df.loc[df['Algorithm'] == 'Classic']
 
