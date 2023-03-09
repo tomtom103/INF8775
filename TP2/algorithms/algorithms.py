aac02f223705cbeef9e4e2ce4039473d46d6ac3a
Thomas Caron <thomas.caron00@gmail.com>
2023-03-09 15:46:14 -0500

    Moved plotting utilities to utils.py

--- TP2/algorithms/algorithms.py
+++ TP2/algorithms/algorithms.py
@@ -1,30 +1,6 @@
-import itertools
-from typing import List, Tuple
+from typing import List
 
-import matplotlib.pyplot as plt
-
-from .utils import City, path_cost, distanceGraph
-
-def _init_plot(cities: List[City]) -> None:
-    fig = plt.figure(0)
-    fig.suptitle("Greedy algorithm")
-    x_lst, y_lst = [], []
-    for city in cities:
-        x_lst.append(city.x)
-        y_lst.append(city.y)
-    x_lst.append(cities[0].x)
-    y_lst.append(cities[0].y)
-
-    plt.plot(x_lst, y_lst, 'ro')
-    plt.show(block=False)
-
-def plot_interactive(route: List[City], block: bool = False):
-    x1, y1, x2, y2 = route[-2].x, route[-2].y, route[-1].x, route[-1].y
-    plt.plot([x1, x2], [y1, y2], 'ro')
-    plt.plot([x1, x2], [y1, y2], 'g')
-    plt.draw()
-    plt.pause(0.07)
-    plt.show(block=block)
+from .utils import City, path_cost, init_plot, plot_interactive
     
 
 def greedy(cities: List[City], plot: bool = False):
@@ -32,9 +8,7 @@ def greedy(cities: List[City], plot: bool = False):
     route = [cities[0]]
 
     if plot:
-        plt.ion()
-        plt.show(block=False)
-        _init_plot([*route, *unvisited])
+        init_plot([*route, *unvisited], "Greedy Algorithm")
 
     while len(unvisited):
         index, city = min(
--- TP2/algorithms/utils.py
+++ TP2/algorithms/utils.py
@@ -3,7 +3,11 @@ import os
 from typing import List, Tuple, Union
 from dataclasses import dataclass
 
+import matplotlib.pyplot as plt
+
+
 _PathLike = Union[str, bytes, os.PathLike]
+
 
 @dataclass
 class City:
@@ -15,6 +19,7 @@ class City:
     
     def __repr__(self) -> str:
         return f"({self.x}, {self.y})"
+
 
 def read_cities(file: _PathLike) -> Tuple[int, List[City]]:
     cities: List[City] = []
@@ -32,5 +37,31 @@ def read_cities(file: _PathLike) -> Tuple[int, List[City]]:
 def distanceGraph(cities: List[City]) -> List[List[float]]:
     return [[x.distance(y) for y in cities] for x in cities]
 
+
 def path_cost(route: List[City]) -> float:
     return sum([city.distance(route[idx - 1]) for idx, city in enumerate(route)])
+
+
+def init_plot(cities: List[City], title: str) -> None:
+    plt.ion()
+    plt.show(block=False)
+    fig = plt.figure(0)
+    fig.suptitle(title)
+    x_lst, y_lst = [], []
+    for city in cities:
+        x_lst.append(city.x)
+        y_lst.append(city.y)
+    x_lst.append(cities[0].x)
+    y_lst.append(cities[0].y)
+
+    plt.plot(x_lst, y_lst, 'ro')
+    plt.show(block=False)
+
+
+def plot_interactive(route: List[City], block: bool = False):
+    x1, y1, x2, y2 = route[-2].x, route[-2].y, route[-1].x, route[-1].y
+    plt.plot([x1, x2], [y1, y2], 'ro')
+    plt.plot([x1, x2], [y1, y2], 'g')
+    plt.draw()
+    plt.pause(0.07)
+    plt.show(block=block)
