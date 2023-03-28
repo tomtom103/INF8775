d2eff68a2d10f2121bdcc8f543f36732fbc181da
Thomas Caron <thomas.caron00@gmail.com>
2023-03-28 15:25:20 -0400

    Added function to read input files

--- TP3/main.py
+++ TP3/main.py
@@ -1,5 +1,39 @@
 import argparse
+import os
 from pathlib import Path
+
+import numpy as np
+
+def read_file(file: Path) -> ...:
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    n, m, k = list(map(lambda x: int(x), lines[0].split(" ")))
+    print(f"n = {n}, m = {m}, k = {k}")
+    
+    liste_enclos = list(map(lambda x: int(x), lines[1].split(" ")))
+    print(f"Liste des enclos: {liste_enclos}")
+
+    tailles_enclos = []
+    for i in range(2, n + 2):
+        tailles_enclos.append(int(lines[i].strip()))
+
+    print(f"Taille des enclos: {tailles_enclos}")
+
+    # Remaining n lines
+    # n lignes representent les poids de l'enclos i vers les n autre enclos
+    # on a un poids de zero pour une distance avec soi-meme
+    lines = lines[i + 1:]
+    
+    matrice_poids = []
+    for line in lines:
+        vec_ligne = []
+        for val in line.split(" "):
+            vec_ligne.append(int(val))
+
+        matrice_poids.append(vec_ligne)
+
+    print(f"Matrice de poids: \n{np.matrix(matrice_poids)}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -9,7 +43,7 @@ if __name__ == "__main__":
                         help="Affiche les indices des villes a visiter en commencant par 0 et finissant par 0")
     args = parser.parse_args()
 
-    fichier = Path(str(args.e))
+    read_file(Path(str(args.e)))
 
     if bool(args.p):
         # A chaque fois qu'une meilleure solution est trouvée, affiche la nouvelle solution. Format décrit dans le rapport
