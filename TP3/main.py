850c7b7413cecf1c4152c097e9af8a8f01384b31
Thomas Caron <thomas.caron00@gmail.com>
2023-03-30 10:52:39 -0400

    Added graph representation of the problem

--- TP3/main.py
+++ TP3/main.py
@@ -1,38 +1,50 @@
 import argparse
-import os
 from pathlib import Path
 
 import numpy as np
 
-def read_file(file: Path) -> ...:
+from utils import Graph
+
+def read_file(file: Path) -> Graph:
+    g = Graph()
+
     with open(file, 'r') as f:
         lines = f.readlines()
+    
+    # n: nombre d'enclos
+    # m: nombre d'enclos dans le sous-ensemble S
+    # k: la distance maximale a respecter pour les enclos du sous-ensemble
     n, m, k = list(map(lambda x: int(x), lines[0].split(" ")))
     print(f"n = {n}, m = {m}, k = {k}")
     
-    liste_enclos = list(map(lambda x: int(x), lines[1].split(" ")))
-    print(f"Liste des enclos: {liste_enclos}")
+    # Liste d'enclos a placer a une distance de k pour obtenir
+    # le bonus V = m^2
+    bonus_enclosures = list(map(lambda x: int(x), lines[1].split(" ")))
+    print(f"Liste des enclos a placer pour bonus: {bonus_enclosures}")
 
-    tailles_enclos = []
-    for i in range(2, n + 2):
-        tailles_enclos.append(int(lines[i].strip()))
+    # Tailles des enclos i pour i allant de 0 a n-1
+    enclosure_size = [int(lines[i].strip()) for i in range(2, n + 2)]
 
-    print(f"Taille des enclos: {tailles_enclos}")
+    for i in range(n):
+        g.add_vertex(i, enclosure_size[i])
 
-    # Remaining n lines
+    assert len(enclosure_size) == n
+
+    print(f"Taille des enclos: {enclosure_size}")
+
     # n lignes representent les poids de l'enclos i vers les n autre enclos
-    # on a un poids de zero pour une distance avec soi-meme
-    lines = lines[i + 1:]
-    
-    matrice_poids = []
-    for line in lines:
-        vec_ligne = []
-        for val in line.split(" "):
-            vec_ligne.append(int(val))
+    # on a un poids de zero pour une distance avec soi-meme    
+    weight_matrix = [[int(val) for val in line.split(" ")] for line in lines[n + 2:]]
 
-        matrice_poids.append(vec_ligne)
+    for i in range(n):
+        for j in range(n):
+            if weight_matrix[i][j] != 0: # if [i][j] == 0 then [j][i] == 0 because diagonal
+                g.add_edge(i, j, weight_matrix[i][j])
+                g.add_edge(j, i, weight_matrix[j][i])
 
-    print(f"Matrice de poids: \n{np.matrix(matrice_poids)}")
+    print(f"Matrice de poids: \n{np.matrix(weight_matrix)}")
+
+    return g
 
 
 if __name__ == "__main__":
@@ -43,9 +55,11 @@ if __name__ == "__main__":
                         help="Affiche les indices des villes a visiter en commencant par 0 et finissant par 0")
     args = parser.parse_args()
 
-    read_file(Path(str(args.e)))
+    g = read_file(Path(str(args.e)))
 
-    if bool(args.p):
+    p = bool(args.p)
+
+    if p:
         # A chaque fois qu'une meilleure solution est trouvée, affiche la nouvelle solution. Format décrit dans le rapport
         ...
     else:
