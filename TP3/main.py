11fca08755aa33212c42973e37fd89822b2cdcdb
Thomas Caron <thomas.caron00@gmail.com>
2023-04-15 19:09:18 -0400

    Updated main.py for basic script interface

--- TP3/main.py
+++ TP3/main.py
@@ -1,51 +1,67 @@
-import argparse
-from pathlib import Path
-
 import numpy as np
+import argparse
+import math
+from pathlib import Path
+from typing import List, Tuple, Dict, Iterator
+from dataclasses import dataclass, field
 
-from utils import Graph
+@dataclass
+class Enclosure:
+    id: int
+    size: int
+    shape: List[Tuple[int, int]] # [(x, y), (x, y)]
+    neighbors: List[Tuple["Enclosure", int]] # Enclosure + weight to that enclosure
 
-def read_file(file: Path) -> Graph:
-    g = Graph()
 
-    with open(file, 'r') as f:
-        lines = f.readlines()
-    
-    # n: nombre d'enclos
-    # m: nombre d'enclos dans le sous-ensemble S
-    # k: la distance maximale a respecter pour les enclos du sous-ensemble
-    n, m, k = list(map(lambda x: int(x), lines[0].split(" ")))
-    print(f"n = {n}, m = {m}, k = {k}")
-    
-    # Liste d'enclos a placer a une distance de k pour obtenir
-    # le bonus V = m^2
-    bonus_enclosures = list(map(lambda x: int(x), lines[1].split(" ")))
-    print(f"Liste des enclos a placer pour bonus: {bonus_enclosures}")
+def pack_enclosures(enclosures: List[Enclosure], bonus_enclosures: List[Enclosure], weights: List[List[int]]) -> Iterator[List[List[int]]]:
+    total_area = sum([enclosure.size for enclosure in enclosures])
 
-    # Tailles des enclos i pour i allant de 0 a n-1
-    enclosure_size = [int(lines[i].strip()) for i in range(2, n + 2)]
+    # Calculate the width and height of the bounding box
+    width = int(math.sqrt(total_area))
+    height = int(math.ceil(total_area / width))
 
-    for i in range(n):
-        g.add_vertex(i, enclosure_size[i])
+    while True: # TODO: Replace with time constraints instead
+        bbox = [[0 for _ in range(width)] for _ in range(height)]
 
-    assert len(enclosure_size) == n
+        for enclosure in enclosures:
+            # TODO: Logic to place an enclosure within the box
+            ...
 
-    print(f"Taille des enclos: {enclosure_size}")
+            # assert len(enclosure.shape) == enclosure.size
+            for x, y in enclosure.shape:
+                bbox[y][x] = enclosure.id
 
-    # n lignes representent les poids de l'enclos i vers les n autre enclos
-    # on a un poids de zero pour une distance avec soi-meme    
-    weight_matrix = [[int(val) for val in line.split(" ")] for line in lines[n + 2:]]
+        # TODO: Instead of yielding every time, only yield when we found a solution that is better than the previous one
+        yield bbox
 
-    for i in range(n):
-        for j in range(n):
-            if weight_matrix[i][j] != 0: # if [i][j] == 0 then [j][i] == 0 because diagonal
-                g.add_edge(i, j, weight_matrix[i][j])
-                g.add_edge(j, i, weight_matrix[j][i])
+def populate_most_valuable_neighbors(enclosures: List[Enclosure], bonus_enclosures: List[Enclosure], weights: List[List[int]]) -> List[Enclosure]:
+    # TODO: Do shit here
 
-    print(f"Matrice de poids: \n{np.matrix(weight_matrix)}")
+    for enclosure in enclosures:
+        # TODO: Figure out how to populate the neighbors according to weights and bonus enclosures that need to be together
+        # We could probably want some kind of heuristic here that tells us which enclosures absolutely need to be next to eachother
+        # If we have a score of like 30 vs 300, the 300 one obviously takes priority
+        enclosure.neighbors = []
 
-    return g
+    # Return all enclosures with a populated list of neighbors (IMPORTANT)
+    return enclosures
 
+
+def read_file(file_path: Path, p: bool):
+    with open(file_path, 'r') as file:
+        n, m, k = map(int, file.readline().split())
+        print(f"n = {n}, m = {m}, k = {k}")
+
+        bonus_enclosures = list(map(int, file.readline().split()))
+        print(f"Liste des enclos a placer pour bonus: {bonus_enclosures}")
+
+        enclosure_sizes = [int(file.readline()) for _ in range(n)]
+        print(f"Taille des enclos: {enclosure_sizes}")
+
+        enclosure_weights = [list(map(int, file.readline().split())) for _ in range(n)]
+        print(f"Matrice de poids: \n{np.matrix(enclosure_weights)}")
+
+    return n, m, k, bonus_enclosures, enclosure_sizes, enclosure_weights
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -55,13 +71,15 @@ if __name__ == "__main__":
                         help="Affiche les indices des villes a visiter en commencant par 0 et finissant par 0")
     args = parser.parse_args()
 
-    g = read_file(Path(str(args.e)))
-
     p = bool(args.p)
 
-    if p:
-        # A chaque fois qu'une meilleure solution est trouvée, affiche la nouvelle solution. Format décrit dans le rapport
-        ...
-    else:
-        # Afficher seulement l'attrait total du zoo à chaque fois qu'une meilleure solution est trouvée.
+    n, m, k, bonus_enclosures, enclosure_sizes, enclosure_weights = read_file(Path(str(args.e)), p)
+
+    enclosures = [Enclosure(id=i, size=enclosure_sizes[i]) for i in range(n)]
+    bonus_enclosures = [enclosures[i] for i in bonus_enclosures]
+
+    enclosures = populate_most_valuable_neighbors(enclosures, bonus_enclosures, enclosure_weights)
+
+    for bbox in pack_enclosures(enclosures, bonus_enclosures, enclosure_weights):
+        # TODO: Find a way to properly print the bbox on each iteration
         ...
